4e96c7ec8c402a8483eff8cf4ec024d844124b10
migmolrod <migmolrod@fastmail.es>
2025-11-18 16:37:16 +0100

    Refactor styles and improve metadata rendering
    - Updated `RequirementMetadata` to use a `formatDate` utility for
      consistent date formatting.
    - Refined styles for `PriorityLabel`, `StatusLabel`, and
      `RequirementMetadata` components for enhanced appearance and theme
      alignment.
    - Applied consistent heading styles across the documentation.

--- docs/src/components/RequirementMetadata/index.tsx
+++ docs/src/components/RequirementMetadata/index.tsx
@@ -1,5 +1,10 @@
-import styles from "../StatusLabel/styles.module.css";
+import styles from "./styles.module.css";
 import StatusLabel from "@site/src/components/StatusLabel";
+
+function formatDate(date: Date): string {
+	const formatter = new Intl.DateTimeFormat('en-US', {dateStyle: 'long'});
+	return formatter.format(date);
+}
 
 export default function RequirementMetadata({status, lastUpdated}) {
 	return (
@@ -8,7 +13,7 @@ export default function RequirementMetadata({status, lastUpdated}) {
 				<strong>Status: </strong>
 				<StatusLabel status={status}/>
 			</div>
-			{lastUpdated && <span><strong>Last updated</strong>: {lastUpdated.toLocaleDateString()}</span>}
+			{lastUpdated && <span><strong>Last updated</strong>: {formatDate(lastUpdated)}</span>}
 			{/*{reviewers?.length > 0 && <span>Reviewers: {reviewers.join(", ")}</span>}*/}
 		</div>
 	);
