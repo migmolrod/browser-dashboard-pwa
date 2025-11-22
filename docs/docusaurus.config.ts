1bf1acc391492236a65af1ce04504660cd55ddd4
migmolrod <migmolrod@fastmail.es>
2025-11-22 16:41:22 +0100

    Fix docusaurus baseUrl for GitHub Pages

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
 	url: "https://migmolrod.github.io",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+	baseUrl: "/browser-dashboard-pwa/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
